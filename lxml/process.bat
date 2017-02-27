8f95ff8392bc222dbb0d8c5528cb14886c240bd4
74m15 <pasquale.capuano@gmail.com>
2017-02-27 17:31:18 +0100

    First dirty support for web interface

--- lxml/process.bat
+++ lxml/process.bat
@@ -1,17 +1,33 @@
 @echo off
 
 set IN="template.docx"
+set OUT="new.template.docx"
+set DB="database.xml"
 set DUMMY=0
 set LOG=1
 
 :parse_args
 if "%1"=="-in" goto set_in
+if "%1"=="-out" goto set_out
+if "%1"=="-db" goto set_db
 if "%1"=="-dummy" goto set_dummy
 if "%1"=="-log" goto set_log
 goto GO_XSL
 
 :set_in
 set IN=%2
+shift
+shift
+goto parse_args
+
+:set_out
+set OUT=%2
+shift
+shift
+goto parse_args
+
+:set_db
+set DB=%2
 shift
 shift
 goto parse_args
@@ -28,34 +44,33 @@ shift
 goto parse_args
 
 :GO_XSL
-set IN_new=new.%IN%
-set IN_new_dir=%IN_new%.dir
+set OUT_dir=%OUT%.dir
 
 echo IN     = %IN%
-echo IN_new = %IN_new%
+echo OUT    = %OUT%
 echo DUMMY  = %DUMMY%
 echo LOG    = %LOG%
 
 echo Unzipping document...
-cp work\document\%IN% work\document\%IN_new%
-unzip -d work\document\%IN_new_dir% work\document\%IN_new%
+cp work\document\%IN% work\document\%OUT%
+unzip -d work\document\%OUT_dir% work\document\%OUT%
 
 echo Processing document...
-set ARGS_1=-in dummy.xml -xsl ./xsl/document_processor.xsl -out "./work/document/%IN_new_dir%/word/new_document.xml"
-set ARGS_2=-p log_level=%LOG% -p docx_filename='%IN_new_dir%' -p dummy_fill=%DUMMY%
+set ARGS_1=-in dummy.xml -xsl ./xsl/document_processor.xsl -out "./work/document/%OUT_dir%/word/new_document.xml"
+set ARGS_2=-p log_level=%LOG% -p docx_filename='%OUT_dir%' -p db_filename='%DB%' -p dummy_fill=%DUMMY%
 python xslt.py %ARGS_1% %ARGS_2%
 
 echo Finishing document...
-rm work/document/%IN_new_dir%/word/document.xml
-mv work/document/%IN_new_dir%/word/new_document.xml work/document/%IN_new_dir%/word/document.xml
+rm work/document/%OUT_dir%/word/document.xml
+mv work/document/%OUT_dir%/word/new_document.xml work/document/%OUT_dir%/word/document.xml
 
-pushd work\document\%IN_new_dir%
+pushd work\document\%OUT_dir%
 
-zip -u ..\%IN_new% word\document.xml
+zip -u ..\%OUT% word\document.xml
 
 popd
 
-rm -rf work/document/%IN_new_dir%
+rm -rf work/document/%OUT_dir%
 
 :FINISH
-echo Document "%IN_new%" ready!
+echo Document "%OUT%" ready!
---
+++ lxml/web.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 22 00:03:20 2017
+
+@author: pasquale.capuano@gmail.com
+"""
+
+from bottle import Bottle, run, template, static_file, request
+from uuid import uuid1
+import subprocess
+
+app = Bottle()
+
+@app.get("/")
+def home():
+  return template("www/home.html")
+
+@app.get("/docs/<path:path>")
+def get_docs(path):
+  return static_file(path, root='work/document/')
+  
+@app.post("/upload")
+def do_upload():
+  xlsx = request.files.get("xlsx")
+  db   = request.files.get("db")
+  docx = request.files.get("docx")
+  
+  in_id = str(uuid1())
+  out_id = str(uuid1())
+  
+  if (xlsx):
+    xlsx.save("work\database\{0}.xlsx".format(in_id))
+  
+  if (db):
+    db.save("work\database\{0}.xml".format(in_id))
+    
+  if (docx):
+    docx.save("work\document\{0}.docx".format(in_id))
+  
+  args = [
+    "process.bat", 
+    "-in", "{0}.docx".format(in_id),
+    "-db", "{0}.xml".format(in_id), 
+    "-out", "{0}.docx".format(out_id),
+    "-log", "1"]
+  
+  proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  
+  result = proc.communicate()
+  
+  return template("www/uploaded.html", target="{0}.docx".format(out_id), out=result[0], err=result[1])
+
+run(app, host="0.0.0.0", port=8080, debug=True)
